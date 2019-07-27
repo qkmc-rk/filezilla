736547fd200cdcc55460b4f5e31db413d41409b4
mayberk <qkmc@outlook.com>
2019-07-27 19:35:33 +0800

    first commit

---
+++ src/main/java/xyz/ruankun/filezilla/FilezillaApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.filezilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilezillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilezillaApplication.class, args);
+	}
+
+}
